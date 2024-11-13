1301fcdca09404c3758c51029c3bc78b24879b44
Francielly Souza <francielly1414@gmail.com>
2024-11-13 16:42:51 -0300

    OK

---
+++ aula-13-11/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles.css">
+    <title>Aula 13-11</title>
+</head>
+<body>
+
+    <nav>
+        <ul><li> <a href="https://www.w3schools.com/css/default.asp">GREEN</a></li>
+        <ul><li> <a href="https://www.w3schools.com/css/default.asp">VERDE</a></li>
+        <ul><li> <a href="https://www.w3schools.com/css/default.asp">VERT</a></li>
+        
+        </ul>
+    </nav>
+</body>
+</head>
+
+
+    
