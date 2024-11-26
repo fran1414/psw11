4cbcd52212624bd5827932e1c8d30a2937fb0958
Francielly Souza <francielly1414@gmail.com>
2024-11-26 10:33:12 -0300

    OK

---
+++ aula-26-11/index.php
@@ -0,0 +1,31 @@
+
+<?php
+$var="Bill Gates";
+$tipo= gettype"$var";
+echo "$var .($tipo)<br>";
+$var = 12
+$tipo = gettype($var);
+echo "$var ($tipo)<br>";
+$var=3.1415;
+$tipo =grttype($var);
+echo "$var ($tipo)<br>";
+$var =true;
+$tipo= gettype($var);
+echo"$var ($tipo)<br>";
+?>
+<?php 
+if(is_null($var)0){
+    echo "Varialvel \$var é nula/vazia!<br>";
+}
+$var = "Bill GAtes";
+if(is_string($var)){
+    echo"Variavel \$var é uma string!<br>";
+}
+$var =12;
+if(is_integer($var)){
+    echo "Variavel \$var é um inteiro!<br>";
+}
+$var = false;
+if(is_bool($var)){
+    echo"Variavel \$var é booleana!";
+}
