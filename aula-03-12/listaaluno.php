8a82ed8204401fd49ae623ab1b82393b3710129e
Francielly Souza <francielly1414@gmail.com>
2024-12-03 10:31:01 -0300

    ok

---
+++ aula-03-12/listaaluno.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+</body>
+</html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cabecalho"2>
+    <h1>TABELA-ALUNOS CADASTRADOS</h1>
+    </div>
+    <div>
+      
+    <table>
+        <tr>
+        <th>NOME</th>
+        <th>ENDEREÇO</th>
+        <th>BAIRRO</th>
+        <th>CIDADE</th>
+        <th>UF</th>
+        <th>CPF</th>
+        <th>TELEFONE</th>
+        <th>CELULAR</th>
+        <th>DATA DE NASCIMENTO</th>
+        <th>DISCIPLINAS CURSADAS</th>
+        <th>DISCIPLINAS PREFERIDAS</th>
+        </tr>
+        <tr>
+            <td>Francielly Da Paz Souza</td>
+            <td>Rua 15 de julho</td>
+            <td>Bairro Porto Alegre</td>
+            <td>Guanambi</td>
+            <td>BA</td>
+           <td>276.898.675-22</td>
+           <td>(77)999061994</td>
+           <td>+55 77999061994</td>
+           <td>14/07/2007</td>
+           <td>Banco de Dados;<br>
+           Programação de Sistemas Web; <br> Projeto Integrador; <br>Análise de Sistemas Web;  <br>Redes de Computadores;</td>
+          <td>Banco de Dados;</td>
+        </tr>
+        <tr>
+             <td>Patrizia Souza Ribeiro </td>
+            <td>Rua Pedro Alves</td>
+            <td>Bairro Jardim das Flores</td>
+            <td>Palmas de Monte Alto</td>
+            <td>BA</td>
+           <td>202.738.995-92</td>
+           <td>(77)999876524</td>
+           <td>+55 77999876524</td>
+           <td>19/10/2007</td>
+           <td>Banco de Dados;<br>
+           Programação de Sistemas Web; <br> Projeto Integrador; <br>
+          <td>Análise de Sistemas Web;</td>
+        </tr>
+        <tr>
+            <td>Yasmin Andrade Souza</td>
+            <td>Rua Jacinto Coelho</td>
+            <td>Bairro Nova Aliança</td>
+            <td>Caculé</td>
+            <td>BA</td>
+           <td>296.563.990-22</td>
+           <td>(77)999082516</td>
+           <td>+55 77999082516</td>
+           <td>11/03/2008</td>
+           <td>Banco de Dados;<br>
+           Programação de Sistemas Web; <br> Projeto Integrador; <br>Análise de Sistemas Web;</td>
+          <td>Projeto Integrador;</td>
+        </tr>
+            
+    </table>
+      
+    </div>
+    <div class="rodape2">
+      <h5>Instituto Federal de Educação, Ciência e Tecnologia da Bahia - Campus Guanambi</h5>
+Telefone: (77) 0000-0000 | E-mail: contato@ifba.edu.br
+www.ifba.edu.br
+
+</p>
+      
+    </div>
+</body>
+</html>
