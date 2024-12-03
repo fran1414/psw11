50747d3b313bade39ff92da1b6087a0f98aea6c7
Francielly Souza <francielly1414@gmail.com>
2024-12-03 10:50:47 -0300

    ok

--- aula-03-12/cadaluno.php
+++ aula-03-12/cadaluno.php
@@ -2,7 +2,7 @@
              <title> Formulário - POST </title>  
              <meta charset="utf-8"> </head> 
                <body> 
-                 <form action="listaaluno" method="post"> 
+                 <form action="listaaluno.php" method="post"> 
                           <p> Aluno: <input type="text" name="aluno"/> </p>  
                            <p> Turma: <input type="text" name="turma"/> </p>  
                              <p> <input type="submit" value="Enviar!"/> </p>    
