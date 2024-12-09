3b49d825b60cbda8640f9cde610e8f9a03a8255a
Francielly Souza <francielly1414@gmail.com>
2024-12-09 17:17:40 -0300

    ok

---
+++ atv03-framcielly-souza/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Aula sobre</title>
+</head>
+<body>
+  <div>
+    <div class="titulo">
+        <div id="dif1"><img src="image1" width="30%" height="30%"></div>
+        <div id="dif2"><h1>SISTEMA IF BAIANO-Campus Guanambi</h1></div>
+          <div id="dif3"> </div>
+        </div>
+    <div class="nav">
+<ul>
+  <li><a href="index.html">PÁGINA INICIAL</a></li>
+  <li><a href="cadaluno.html">CADASTRO DE ALUNOS</a></li>
+  <li><a href="listaaluno.html">LISTA DE ALUNOS</a></li>
+  </div>
+  
+  </ul>
+    
+  </div>
+  
+
+<div class="center">
+    <div id="div1"><p>O Instituto Federal Baiano (IF Baiano) – Campus Guanambi foi uma das unidades das antigas Escolas Agrotécnicas Federais (EAFs) incorporadas ao Instituto na criação dos Institutos Federais no Brasil pela Lei n. 11.892, de 29 de dezembro de 2008.
+
+Historicamente, nos anos noventa, criou-se a Escola Agrotécnica Antônio José Teixeira (Lei n. 8.670 de 1993) e foi transformada em autarquia com autonomia disciplinar e acadêmica e seu próprio quadro de pessoal e orçamento (Lei n° 8.731 de 1993).
+
+Situado dentro do Território de Identidade Sertão Produtivo, o campus forma profissionais e contribui com a melhoria dos produtos regionais, na qualificação e na assessoria técnica prestada aos pequenos e médios produtores rurais.</p><br><p>O Ensino viabiliza a formação humana e integral de indivíduos para o desenvolvimento profissional; atua com a verticalização dos estudos (educação básica a superior) por meio de cursos presenciais e à distância; planeja, acompanha, desenvolve, implementa e avalia os processos de educação que se baseiam nos princípios de interdisciplinaridade, transdisciplinaridade, relações étnico-raciais, educação ambiental, inclusão, sustentabilidade, multiculturalidade e igualdade de condições sociais voltados às demandas locais e regionais. </p></div>
+<div id="div2"><img src="Sem título.jpeg" width="530px"height="250px">
+    <br>
+    <img src="título.jpeg" alt="Alunos do curso Informática p/ Internet são selecionados à participarem no Reino Unido do projeto Educadrones do IF Baiano Campus Guanambi" width="530px" height="350px"></div>
+</div>
+    <div class="rodape">
+  <p>
+  Instituto Federal de Educação, Ciência e Tecnologia da Bahia - Campus Guanambi <br>
+    Telefone: (77) 0000-0000 | E-mail: contato@ifba.edu.br www.ifba.edu.br </p>
+</div>
+  
+</div>
+
+</body>
+</html>
